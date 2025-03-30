b12c13a01dc371b32ecfd71fa26cd536c90d7d68
罗杰 <rogerdevworld@gmail.com>
2025-03-31 01:44:43 +0200

    ft_printf.c [OK], norminette [OK]

--- src/mandatory/ft_printf.c
+++ src/mandatory/ft_printf.c
@@ -6,12 +6,12 @@
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 14:06:47 by rmarrero          #+#    #+#             */
-/*   Updated: 2024/11/26 16:23:01 by rmarrero         ###   ########.fr       */
+/*   Updated: 2025/01/24 16:39:25 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "../../include/ft_printf.h"
+#include "../include/printf.h"
 
-/* dejare el ft_printf para manejo de errores 
+/* dejare el ft_printf para manejo de errores
  * etc y llamar a las funciones finales */
 int	ft_printf(const char *str, ...)
 {
@@ -20,31 +20,23 @@ int	ft_printf(const char *str, ...)
 	int		length;
 	int		flag;
 
-	i = 0;
+	i = -1;
 	length = 0;
 	flag = 1;
 	va_start(args, str);
-	if (!*str)
-		return (0);
-	while (str[i])
+	while (str[++i])
 	{
-		if (str[i] == '%')
+		if (str[i] == '%' && str[++i])
 		{
-			i++;
-			if (str[i])
-				if (ft_validation(str[i]) == 1)
-					is_flag(str[i], args, &length, &flag);
-				else
-					return (-1);
+			if (ft_validation(str[i]) == 1)
+				is_flag(str[i], args, &length, &flag);
 			else
-				break ;
+				return (-1);
 		}
 		else
 			ft_putchar(str[i], &length, &flag);
 		if (flag == -1)
 			return (-1);
-		i++;
 	}
-	va_end(args);
-	return (length);
+	return (va_end(args), length);
 }
