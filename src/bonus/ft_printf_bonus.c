acea5bc262ecbeacf518268366516cb42ac27194
罗杰 - Roger <rmarrero@cbr1s4.42barcelona.com>
2025-07-23 03:05:14 +0200

    src/bonus/ft_printf_bonus.c

--- src/bonus/ft_printf_bonus.c
+++ src/bonus/ft_printf_bonus.c
@@ -11,11 +11,38 @@
 /* ************************************************************************** */
 #include "../../include/ft_printf_bonus.h"
 
+static void	handle_format(const char **str, va_list args, int *length)
+{
+	t_printf	ft_flags;
+
+	ft_initialise_flags(&ft_flags);
+	ft_apply_flags(str, &ft_flags);
+	ft_apply_width(str, &ft_flags);
+	ft_apply_precision(str, &ft_flags);
+	if (ft_validation(**str))
+	{
+		if (**str == 'c')
+			ft_char(&ft_flags, args, length);
+		else if (**str == 's')
+			ft_string(&ft_flags, args, length);
+		else if (**str == 'd' || **str == 'i')
+			ft_integer(&ft_flags, args, length);
+		else if (**str == 'u')
+			ft_unsigned(&ft_flags, args, length);
+		else if (**str == 'p')
+			ft_pointer(&ft_flags, args, length);
+		else if (**str == 'x' || **str == 'X')
+			ft_hex(&ft_flags, args, length, **str);
+		else if (**str == '%')
+			ft_percent(&ft_flags, length);
+		(*str)++;
+	}
+}
+
 int	ft_printf(const char *str, ...)
 {
-	va_list		args;
-	t_printf	ft_flags;
-	int			length;
+	va_list	args;
+	int		length;
 
 	length = 0;
 	va_start(args, str);
@@ -24,28 +51,7 @@ int	ft_printf(const char *str, ...)
 		if (*str == '%')
 		{
 			str++;
-			ft_initialise_flags(&ft_flags);
-			ft_apply_flags(&str, &ft_flags);
-			ft_apply_width(&str, &ft_flags);
-			ft_apply_precision(&str, &ft_flags);
-			if (ft_validation(*str))
-			{
-				if (*str == 'c')
-					ft_char(&ft_flags, args, &length);
-				else if (*str == 's')
-					ft_string(&ft_flags, args, &length);
-				else if (*str == 'd' || *str == 'i')
-					ft_integer(&ft_flags, args, &length);
-				else if (*str == 'u')
-					ft_unsigned(&ft_flags, args, &length);
-				else if (*str == 'p')
-					ft_pointer(&ft_flags, args, &length);
-				else if (*str == 'x' || *str == 'X')
-					ft_hex(&ft_flags, args, &length, *str);
-				else if (*str == '%')
-					ft_percent(&ft_flags, &length);
-				str++;
-			}
+			handle_format(&str, args, &length);
 		}
 		else
 			ft_putchar(*str++, &length);
